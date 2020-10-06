6d9275585449b05aee9e647f4f7b859dfadbf32f
Ary-Asyari <aryasyaricoc1@gmail.com>
2020-10-05 21:30:09 -0500

    merubah line 7 menjadi product, line 8 article

--- HelloWorld.go
+++ HelloWorld.go
@@ -4,7 +4,6 @@ import "fmt"
 
 func main() {
 	fmt.Println("membuat halaman beranda")
-	fmt.Println("membuat halaman contact")
-	fmt.Println("membuat halaman about")
+	fmt.Println("membuat halaman product")
+	fmt.Println("membuat halaman article")
 }
-
